e346e0b05329f07cd305b9b5765d4dbe0496d22d
SHAH-JI <shahji1998@gmail.com>
2019-04-29 02:13:01 +0500

    Create image_interpolation_griddata.py

---
+++ image_interpolation_griddata.py
@@ -0,0 +1,29 @@
+% image interpolation using scipy.interpolation.griddata
+import os
+import sys
+import numpy as np
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
+from scipy import interpolate
+
+data = pd.read_csv( "data.csv")
+print (data.describe())
+data=data.sort_values(by=["x","y"],axis=0)
+data =data.reset_index(drop=True)
+print (np.where(data["x"]==100)[0][0]-1)
+
+from scipy.misc import imsave
+def make_interpolated_image(data):
+    ix = data["x"]
+    iy = data["y"]
+    samples =data["f(x,y)"]
+    int_im = griddata((ix, iy), samples, (X, Y),fill_value=0)
+    return int_im
+X, Y = np.meshgrid(np.arange(0, data["x"].max()+1, 1), np.arange(0, data["y"].max()+1, 1))
+matrix =make_interpolated_image(data)
+for i in range(1000):
+    for j in range(2000):
+        matrix[i][j]= matrix[i][j] % 256
+imsave('my.png',matrix)
